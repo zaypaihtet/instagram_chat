40f4781119a0b19054f0e958bf945cf5b96bb9b6
ZayPaiHtet <67455331+zaypaihtet@users.noreply.github.com>
2026-06-22 02:15:28 +0630

    feat: route publisher page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Publisher from "./Publisher";
 import "./styles.css";
 
+const page = window.location.pathname.startsWith("/publisher") ? (
+  <Publisher />
+) : (
+  <App />
+);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode>{page}</React.StrictMode>,
 );
